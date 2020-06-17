032af6003b22cd814f4ff6fbf28c88fcf9a5de54
diavele86 <valentina.castillo86@gmail.com>
2020-06-16 21:55:44 -0500

    answer5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_sign - Entry point
+ *@n: chararter to check
+ * Return: if char greater than 0, return 1, equal to 0 return 0, else -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
