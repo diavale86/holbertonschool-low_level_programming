5eb04e2e46baa1ec5a2ce66811b9f041c67fcda5
diavele86 <valentina.castillo86@gmail.com>
2020-06-18 01:53:07 -0500

    answer3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- *  _islowert - check the code for Holberton School students.
- *@c: if is appercase return 0, else return 1
+ * _islower - check the code for Holberton School students.
+ * @c: if is appercase return 0, else return 1
  * Return: Always 0
  */
 int _islower(int c)
