fc76ce93154ce76b50a06ae633fcda5096479e8d
diavele86 <valentina.castillo86@gmail.com>
2020-06-18 18:51:46 -0500

    Multiple mains - Like, comment, subscribe - 2. 0 > 972?

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int mount, int day, int year);
+int covert_day(int mount, int day);
 
 #endif /* HOLBERTON_H */
