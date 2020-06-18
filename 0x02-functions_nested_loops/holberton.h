aaa6080c3befddce20add8852593ae780b01b26c
diavele86 <valentina.castillo86@gmail.com>
2020-06-18 00:13:24 -0500

    holberton prototipo

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,8 @@
+#ifndef FUNCIONES_BLUCLES_ANIDADO
+#define FUNCIONES_BLUCLES_ANIDADO
+
+void print_to_98(int n);
+int add(int, int);
 void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
@@ -8,3 +13,5 @@ int _islower(int c);
 void print_alphabet_x10(void);
 void print_alphabet(void);
 int _putchar(char c);
+
+#endif
