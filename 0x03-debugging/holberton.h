afe3db85df8bde0d86b49d6fe19eaf9bad68b3e4
diavele86 <valentina.castillo86@gmail.com>
2020-06-19 10:17:06 -0500

    answer3

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,6 +6,6 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int mount, int day, int year);
-int covert_day(int mount, int day);
+int convert_day(int mount, int day);
 
 #endif /* HOLBERTON_H */
