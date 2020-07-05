9da9ae935c29412ad5bc18779f4b257fb0bd0415
diavele86 <valentina.castillo86@gmail.com>
2020-07-04 21:39:37 -0500

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - function
+ *@argc: The number of command line arguments
+ *@argv: containing the program command line arguments
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
