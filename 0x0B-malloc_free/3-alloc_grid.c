12da1030dd088473d72ce8c33ef6d291394dc3bb
diavele86 <valentina.castillo86@gmail.com>
2020-07-09 16:12:55 -0500

    function that returns a pointer

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *alloc_grid - function that returns a pointer
+ *@width: s
+ *@height: j
+ *Return: grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int s;
+	int j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = (int **)malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (s = 0; s < height; s++)
+	{
+		grid[s] = (int *)malloc(sizeof(int *) * width);
+		if (grid[s] == NULL)
+		{
+			for (s = s - 1; s >= 0; s--)
+				free(grid[s]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[s][j] = 0;
+	}
+	return (grid);
+}
