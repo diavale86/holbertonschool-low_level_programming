d9319c5c005ffe8a1794f4bd5acde5ecf17eaec7
diavele86 <valentina.castillo86@gmail.com>
2020-07-15 21:27:06 -0500

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include "3-calc.h"
+
+/**
+ *op_add - add
+ *@a:variable 1
+ *@b:variable 2
+ *Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+
+
+/**
+ *op_sub - subtraction
+ *@a:variable 1
+ *@b:variable 2
+ *Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+		return (a - b);
+}
+
+/**
+ *op_mul - multipli
+ *@a:variable 1
+ *@b:variable 2
+ *Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+		return (a * b);
+}
+
+/**
+ *op_div - div
+ *@a:variable 1
+ *@b:variable 2
+ *Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	if (b  == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+
+/**
+ *op_mod - modulo
+ *@a:variable 1
+ *@b:variable 2
+ *Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b  == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
