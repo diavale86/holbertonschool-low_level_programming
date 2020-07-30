a4eb151eb4e7723df87abd83c8d20ad1807228ff
diavele86 <valentina.castillo86@gmail.com>
2020-07-29 23:22:35 -0500

    function that returns the number

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *listint_len - function that returns the number of elements in a linked
+ *@h: elements of structure
+ *Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
