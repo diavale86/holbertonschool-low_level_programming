06764fc52f605004293883cd011ff42a5bb8e3ad
diavele86 <valentina.castillo86@gmail.com>
2020-07-31 19:02:26 -0500

    function that frees

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - function that free
+ *@head: fist elemen node
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *nodo;
+
+	nodo = *head;
+
+	if (head != NULL)
+	{
+		while (nodo != NULL)
+		{
+			*head = nodo->next;
+			free(nodo);
+			nodo = *head;
+		}
+	}
+}
